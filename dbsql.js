75cedd954c70910f058c2b7e514c493d580221c8
faberyx <fabrizio.catitti@gmail.com>
2016-04-02 10:13:45 +0200

    added db sqliser module

---
+++ dbsql.js
@@ -0,0 +1,46 @@
+var Sequelize = require('sequelize');
+var sqlizr = require('sqlizr');
+
+
+exports.register = (plugin, options, next) => {
+
+
+    // default directory for models
+    options.models = options.models || 'models/**/*.js';
+    var sequelize = new Sequelize('vtiger', 'root', 'asdasd', {
+        host: '91.205.173.98',
+        dialect: 'mysql',
+
+        pool: {
+            max: 5,
+            min: 0,
+            idle: 10000
+        },
+
+    });
+    // test the database connection
+    sequelize.authenticate()
+        .then(function() {
+            // import models
+            sqlizr(sequelize, options.models);
+
+            var db = {
+                sequelize: sequelize,
+                Sequelize: Sequelize
+            };
+
+            // make available in hapi application
+            plugin.expose('db', db);
+
+            return next();
+        })
+        .catch(function(err) {
+            return next(err);
+        });
+
+    
+};
+
+exports.register.attributes = {
+    name: 'dbsql'
+};
