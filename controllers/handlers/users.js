fdd5dba59a23b31da8b978453e7325edb0c40837
faberyx <fabrizio.catitti@gmail.com>
2016-04-06 22:08:18 +0200

    added crud methods

--- controllers/handlers/users.js
+++ controllers/handlers/users.js
@@ -27,7 +27,7 @@ module.exports.create = {
     handler: function(request, reply) {
         
         var m = request.server.plugins.mysql.db.sequelize.models;
-        Task.create({ 
+        m.vtiger_users.create({ 
             user_name: request.params.user_name,
             user_password: request.params.user_password,
             user_hash: request.params.user_hash,
@@ -36,20 +36,55 @@ module.exports.create = {
             last_name:request.params.last_name
          }).then(function(user) {
             reply(user).created('/users/' + user.id ); 
-        })
+        }).catch(function(error) {
+            return reply(Boom.badImplementation(error));
+        });
     }
 }
 
 module.exports.update = {
    // auth: 'jwt',
     handler: function(request, reply) {
-        return reply({ result: 'Restricted!' });
+        
+        var m = request.server.plugins.mysql.db.sequelize.models;
+
+        m.vtiger_users.findById(req.params.id).then(function(user) {
+            if(user)
+                user.update({ 
+                    user_name: request.params.user_name,
+                    user_password: request.params.user_password,
+                    user_hash: request.params.user_hash,
+                    cal_color: request.params.cal_color,
+                    first_name: request.params.first_name,
+                    last_name:request.params.last_name
+                }).then(function(newuser) {
+                    return reply(newuser).created('/users/' + user.id ); 
+                }).catch(function(error) {
+                    return reply(Boom.badImplementation(error));
+                });   
+            else
+                return reply(Boom.notFound());
+        });
+        
+       
     }
 }
 
 module.exports.delete = {
    // auth: 'jwt',
     handler: function(request, reply) {
-        return reply({ result: 'Restricted!' });
+         
+        var m = request.server.plugins.mysql.db.sequelize.models;
+
+        m.vtiger_users.findById(req.params.id).then(function(user) {
+            if(user)
+                user.destroy().then(function(newuser) {
+                    return reply(newuser).created('/users/' + user.id ); 
+                }).catch(function(error) {
+                    return reply(Boom.badImplementation(error));
+                });   
+            else
+                return reply(Boom.notFound());
+        });
     }
 }
