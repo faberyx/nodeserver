349e311d2c0c1ccbb5b71ec1911d1806770417cc
faberyx <fabrizio.catitti@gmail.com>
2016-04-12 08:54:35 +0200

    add fg

---
+++ config/config_dev.1.js
@@ -0,0 +1,9 @@
+var config = {};
+
+config.secret_key = 'secretkey';
+config.db_host = '5.189.150.113';
+config.db_schema = 'vtiger';
+config.db_user = 'root';
+config.db_pass = 'h@p!w03Wg';
+
+module.exports = config;
