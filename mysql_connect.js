7f9e983e7183d39269fd11a6af6554ef8390a4dc
Fabrizio Catitti <fabrizio.catitti@gmail.com>
2016-04-12 14:57:58 +0200

    Update mysql_connect.js

--- mysql_connect.js
+++ mysql_connect.js
@@ -5,43 +5,34 @@ var sqlizr = require('sqlizr');
 var config = require('./config/config');
 
 exports.register = (plugin, options, next) => {
-
+    
     // default directory for models
     options.models = options.models || './models/*.js';
     var sequelize = new Sequelize(config.db_schema, config.db_user, config.db_pass, {
         host: config.db_host,
         dialect: 'mysql',
-        pool: {
-            max: 5,
-            min: 0,
-            idle: 10000
-        },
+        pool: { max: 5, min: 0, idle: 10000 },
         define: {
             freezeTableName: true,
             timestamps: false // true by default
         }
-
     });
+ 
     // test the database connection
     sequelize.authenticate()
         .then(function() {
-            // import models
-            sqlizr(sequelize, options.models);
-
-            var db = {
-                sequelize: sequelize,
-                Sequelize: Sequelize
-            };
-
-            // make available in hapi application
-            plugin.expose('db', db);
-            return next();
+            console.log('db connected');
         })
         .catch(function(err) {
-            return next(err);
+            console.log(err);
         })
-        
-        next();
+    sqlizr(sequelize, options.models);
+
+    var db = { sequelize: sequelize, Sequelize: Sequelize };
+    // make available in hapi application
+    plugin.expose('db', db);
+     
+    next();
 };
 
 exports.register.attributes = {
