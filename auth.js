3c1a22c220eadf4afa0b99b73d24f3a02d902d21
Fabrizio Catitti <fabrizio.catitti@gmail.com>
2016-04-12 12:36:55 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -7,13 +7,12 @@ exports.register = function(plugin, options, next) {
     plugin.auth.strategy('jwt', 'jwt', {
         key: config.secret_key, // Secret key
         verifyOptions: {
-            ignoreExpiration: false,
             algorithms: ['HS256']
         },
         // Implement validation function
         validateFunc: (decoded, request, callback) => {
             var m = request.server.plugins.mysql_connect.db.sequelize.models;
-            m.vtiger_users.findById(decoded.id).then(function(user) {
+            m.vtiger_users.findById(decoded.user.id).then(function(user) {
                 if (user == null) {
                     return callback(null, false);
                 } else {
