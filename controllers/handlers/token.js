7421bab2028b6869e30b1e0c7baa898db1b16f2d
Fabrizio Catitti <fabrizio.catitti@gmail.com>
2016-04-13 15:16:15 +0200

    Update token.js

--- controllers/handlers/token.js
+++ controllers/handlers/token.js
@@ -1,13 +1,23 @@
 const config = require('../../config/config');
 const JWT = require('jsonwebtoken');
 const Boom = require('boom');
+const Joi = require('joi');
+const crypto = require('crypto');
 
 module.exports.getToken = {
+    validate: {
+        payload: {
+            username: Joi.string().min(1).max(256),
+            password: Joi.string().min(1).max(256)
+        }
+    },
     handler: function(request, reply) {
 
         var m = request.server.plugins.mysql_connect.db.sequelize.models;
 
-        m.vtiger_users.findById(request.params.id).then(function(user) {
+        var md5psw = crypto.createHash('md5').update(request.payload.password).digest("hex");
+        
+        m.vtiger_users.findOne({ where: {user_name: request.payload.username,user_hash: md5psw } }).then(function(user) {
             if (user != null) {
                 var token = JWT.sign({user:user.dataValues}, config.secret_key, {
                     expiresIn: 120 // expires in 120 seconds
@@ -16,8 +26,6 @@ module.exports.getToken = {
             } else
                 return reply(Boom.unauthorized('invalid credentials'));
         });
-
-
     }
 };
 
