56899b7d4b84697a63f132abef23c33383d63cb9
Fabrizio Catitti <fabrizio.catitti@gmail.com>
2016-04-14 11:37:54 +0200

    Update routes.js

--- routes.js
+++ routes.js
@@ -4,21 +4,20 @@ exports.register = (plugin, options, next) => {
 
     plugin.dependency('controllers');
 
-    var handlers = plugin.plugins.controllers.handlers;
+    const handlers = plugin.plugins.controllers.handlers;
 
     plugin.route([
+       
         // Application Routes
-      
-        { method: 'GET', path: '/', config: handlers.Default.test },
         { method: 'POST', path: '/token/get', config: handlers.Token.getToken },
         { method: 'GET', path: '/token/check', config: handlers.Token.checkToken },
-        //users
+        //Users
         { method: 'POST', path: '/users', config: handlers.Users.create },
         { method: 'GET', path: '/users', config: handlers.Users.read },
         { method: 'GET', path: '/users/{id}', config: handlers.Users.find },
         { method: 'PUT', path: '/users/{id}', config: handlers.Users.update },
         { method: 'DELETE', path: '/users/{id}', config: handlers.Users.delete },
-        
+        //404
         { method: 'GET', path: '/{path*}', config: handlers.Default.notFound },
     ]);
 
