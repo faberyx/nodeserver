cd8e240a12aae54fae890bf207ca4880934749e6
Fabrizio Catitti <fabrizio.catitti@gmail.com>
2016-04-15 12:05:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const HapiSwagger = require('hapi-swagger');
  
 
 if (process.env.NODE_ENV!='production') {
-    console.log('ss');
+    
     manifest.registrations.push(
         {
             "plugin": {
