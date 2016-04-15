ccea7ea7bcb9cb979905db85c93761346399da82
Fabrizio Catitti <fabrizio.catitti@gmail.com>
2016-04-15 10:46:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
 const Glue = require('glue');
 const Hapi = require('hapi');
 const manifest = require('./config/manifest.json');
+const Inert = require('inert');
+const Vision = require('vision');
+const HapiSwagger = require('hapi-swagger');
+
 
 if (!process.env.PRODUCTION) {
     manifest.registrations.push({
@@ -19,13 +23,32 @@ if (!process.env.PRODUCTION) {
 }
 
 Glue.compose(manifest, { relativeTo: __dirname }, (err, server) => {
+    
     if (err) {
         console.log('server.register err:', err);
     }
-    server.register([require('vision'), require('inert'), { register: require('lout') }], function(err) { 
-        console.log(err);
+    
+    const options = {
+        info: {
+            'title': 'Test API Documentation',
+            'version': '0.0.1',
+        }
+    };
+    
+    server.register([
+        Inert,
+        Vision,
+        {
+            'register': HapiSwagger,
+            'options': options
+        }], (err) => {
+            server.start( (err) => {
+            if (err) {
+                    console.log(err);
+                } else {
+                    console.log('✅  Server is listening on ' + server.info.uri.toLowerCase());
+                }
+            });
     });
-    server.start(() => {
-        console.log('✅  Server is listening on ' + server.info.uri.toLowerCase());
-    });
+ 
 });
